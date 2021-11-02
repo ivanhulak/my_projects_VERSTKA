9ef804dfb32e610e5d7077fc9608045acf6cd984
ivanhulak <g.ivan.gulak17@gmail.com>
2021-11-02 13:47:55 +0200

    Add my lab to collection projects

---
+++ lab4_breed_project/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.testimonials__slider').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        speed: 2500,
+    })
+})
